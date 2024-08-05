ed1c21276720628ba5f5ec7b4f784b6cd3d868aa
TT <ttyang1018@outlook.com>
2024-08-05 08:43:31 +0800

    Update utils.py

--- pytt/utils.py
+++ pytt/utils.py
@@ -354,86 +354,6 @@ def Image2JPG(input_picture_file, output_jpg_file):
 
 
 def GetVideoMetaInfoDictFromVideoFile(video_path):
-    """
-    {
-        'streams': [{
-            'index': 1,
-            'codec_name': 'h264',
-            'codec_long_name': 'H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10',
-            'profile': 'Main',
-            'codec_type': 'video',
-            'codec_time_base': '1/48',
-            'codec_tag_string': 'avc1',
-            'codec_tag': '0x31637661',
-            'width': 3840,
-            'height': 2160,
-            'coded_width': 3840,
-            'coded_height': 2160,
-            'has_b_frames': 0,
-            'sample_aspect_ratio': '1:1',
-            'display_aspect_ratio': '16:9',
-            'pix_fmt': 'yuvj420p',
-            'level': 51,
-            'color_range': 'pc',
-            'color_space': 'bt709',
-            'color_transfer': 'bt709',
-            'color_primaries': 'bt709',
-            'chroma_location': 'left',
-            'refs': 1,
-            'is_avc': 'true',
-            'nal_length_size': '4',
-            'r_frame_rate': '24/1',
-            'avg_frame_rate': '24/1',
-            'time_base': '1/90000',
-            'start_pts': 0,
-            'start_time': '0.000000',
-            'duration_ts': 3138750,
-            'duration': '34.875000',
-            'bit_rate': '4715878',
-            'bits_per_raw_sample': '8',
-            'nb_frames': '837',
-            'disposition': {
-                'default': 1,
-                'dub': 0,
-                'original': 0,
-                'comment': 0,
-                'lyrics': 0,
-                'karaoke': 0,
-                'forced': 0,
-                'hearing_impaired': 0,
-                'visual_impaired': 0,
-                'clean_effects': 0,
-                'attached_pic': 0,
-                'timed_thumbnails': 0
-            },
-            'tags': {
-                'creation_time': '2020-12-15T13:43:07.000000Z',
-                'language': 'und',
-                'handler_name': 'Video track',
-                'encoder': 'biquiti MP4 packer'
-            }
-        }],
-        'format': {
-            'filename':
-            '/Users/lemon/temp/uc/uc/15___Garage_Entry___12_14_2020__4_29_11pm/video/15___Garage_Entry___12_14_2020__4_29_11pm.mp4',
-            'nb_streams': 2,
-            'nb_programs': 0,
-            'format_name': 'mov,mp4,m4a,3gp,3g2,mj2',
-            'format_long_name': 'QuickTime / MOV',
-            'start_time': '0.000000',
-            'duration': '34.875000',
-            'size': '20846727',
-            'bit_rate': '4782044',
-            'probe_score': 100,
-            'tags': {
-                'major_brand': 'mp42',
-                'minor_version': '1',
-                'compatible_brands': 'mp41mp42isom',
-                'creation_time': '2020-12-15T13:43:07.000000Z'
-            }
-        }
-    }
-    """
     try:
         import ffmpeg
     except:
@@ -476,9 +396,6 @@ def FindWordInString(word_to_match, string_to_check) -> str:
 
     return re.compile(r'\b({0})\b'.format(re.escape(word_to_match)),
                       flags=re.IGNORECASE).search(string_to_check)
-
-    # findWholeWord('seek')('those who seek shall find')  # -> <match object>
-    # findWholeWord('word')('swordsmith')  # -> None
 
 
 def LoadJSON(json_file_path):
